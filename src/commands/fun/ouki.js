6ddb00dc881839fe607e63ddd7df3bedd0d7c271
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-03-15 17:46:45 +0900

    Update ouki.js

--- src/commands/fun/ouki.js
+++ src/commands/fun/ouki.js
@@ -2,8 +2,8 @@ const { EmbedBuilder, PermissionsBitField, ActionRowBuilder, ButtonBuilder, Butt
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
 const imageUrls = [
-    "https://example.com/image1.jpg",
-    "https://example.com/image2.jpg",
+    "https://cdn.discordapp.com/attachments/1210452036865499219/1218106273229439006/IMG_7693.webp?ex=660674de&is=65f3ffde&hm=ad26c02b570e0dea17911e7f1f3d2873615e6dad9fc65ee460e5f0020841b8ac&",
+    "https://cdn.discordapp.com/attachments/1210452036865499219/1218106274701643837/IMG_7691.png?ex=660674df&is=65f3ffdf&hm=4451e4c241aaea944db89ce143ca915c2d745c642216926d2ca63a7dc43ebd9f&",
     // Add more URLs as needed
 ];
 
