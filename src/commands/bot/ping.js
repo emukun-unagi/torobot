53bb5226fb2e9ce60c7fa9598e5750000bdfae59
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-03-15 15:14:31 +0900

    Update ping.js

--- src/commands/bot/ping.js
+++ src/commands/bot/ping.js
@@ -1,20 +1,31 @@
 const { EmbedBuilder, PermissionsBitField } = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
- exports.commandBase = {
+exports.commandBase = {
   prefixData: {
-  name: "ping",
-  aliases: ["pong"]
+    name: "ping",
+    aliases: ["pong"]
   },
   slashData: new SlashCommandBuilder()
-  .setName("ping")
-  .setDescription("ping command"),
+    .setName("ping")
+    .setDescription("ping command"),
   cooldown: 5000,
   ownerOnly: false,
   prefixRun: async (client, message, args) => {
-    message.reply(`Pong!`)
+    const ping = Date.now() - message.createdTimestamp;
+    const embed = new EmbedBuilder()
+      .setTitle("Ping Command")
+      .setDescription(`Pong! Latency: ${ping}ms`)
+      .setColor("#ffffff");
+
+    message.reply({ embeds: [embed] });
   },
   slashRun: async (client, interaction) => {
-    interaction.reply(`Pong!`)
+    const ping = Date.now() - interaction.createdTimestamp;
+    const embed = new EmbedBuilder()
+      .setDescription(`${ping}ms`)
+       .setColor("ffffff");
+
+    interaction.reply({ embeds: [embed] });
   }
 }
