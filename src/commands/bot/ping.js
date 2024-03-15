41da75fe0bb5801d5b938143c6eef8262bc33617
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-03-15 16:25:38 +0900

    Update ping.js

--- src/commands/bot/ping.js
+++ src/commands/bot/ping.js
@@ -12,46 +12,34 @@ exports.commandBase = {
     cooldown: 5000,
     ownerOnly: false,
     prefixRun: async (client, message, args) => {
-        const start = Date.now();
-        const ws = client.ws;
-
-        await ws.connect();
-
-        await new Promise(resolve => {
-            ws.on('ready', resolve);
-        });
-
-        const connectTime = Date.now() - start;
-
-        ws.disconnect();
-
+        const ping = client.ws.ping;
+        const messagePing = Date.now() - message.createdTimestamp;
+        const apiPing = client.ws.ping + messagePing;
         const embed = new EmbedBuilder()
-            .setColor(0x0099FF)
-            .setTitle("Pong!")
-            .setDescription(`WebSocket connection time: ${connectTime} ms`)
+            .setColor(0xFFFFFF)
+            .setTitle("pong!")
+            .addFields([
+                { name: "WebSocket ping", value: `${ping} ms`, inline: true },
+                { name: "Message ping", value: `${messagePing} ms`, inline: true },
+                { name: "API ping", value: `${apiPing} ms`, inline: true },
+            ])
             .setTimestamp()
             .setFooter({ text: `Command used by ${message.author.username}`, iconURL: message.author.displayAvatarURL() });
 
         message.reply({ embeds: [embed] });
     },
     slashRun: async (client, interaction) => {
-        const start = Date.now();
-        const ws = client.ws;
-
-        await ws.connect();
-
-        await new Promise(resolve => {
-            ws.on('ready', resolve);
-        });
-
-        const connectTime = Date.now() - start;
-
-        ws.disconnect();
-
+        const ping = client.ws.ping;
+        const messagePing = Date.now() - interaction.createdTimestamp;
+        const apiPing = client.ws.ping + messagePing;
         const embed = new EmbedBuilder()
-            .setColor(0x0099FF)
-            .setTitle("Pong!")
-            .setDescription(`WebSocket connection time: ${connectTime} ms`)
+            .setColor(0xFFFFFF)
+            .setTitle("pong!")
+            .addFields([
+                { name: "WebSocket ping", value: `${ping} ms`, inline: true },
+                { name: "Message ping", value: `${messagePing} ms`, inline: true },
+                { name: "API ping", value: `${apiPing} ms`, inline: true },
+            ])
             .setTimestamp()
             .setFooter({ text: `Command used by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
 
