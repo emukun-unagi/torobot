899dd3d3a8a8e81e00fceba2d87bbc214f86f010
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-03-15 17:06:37 +0900

    Update ouki.js

--- src/commands/fun/ouki.js
+++ src/commands/fun/ouki.js
@@ -4,14 +4,14 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const page1 = new EmbedBuilder()
     .setColor(0x0099FF)
     .setTitle("Page 1")
-    .setImage("https://example.com/image1.jpg")
+    .setImage("https://cdn.discordapp.com/attachments/1210452036865499219/1218106273229439006/IMG_7693.webp?ex=660674de&is=65f3ffde&hm=ad26c02b570e0dea17911e7f1f3d2873615e6dad9fc65ee460e5f0020841b8ac&")
     .setTimestamp()
     .setFooter({ text: "Page 1/2" });
 
 const page2 = new EmbedBuilder()
     .setColor(0x0099FF)
     .setTitle("Page 2")
-    .setImage("https://example.com/image2.jpg")
+    .setImage("https://cdn.discordapp.com/attachments/1210452036865499219/1218106274701643837/IMG_7691.png?ex=660674df&is=65f3ffdf&hm=4451e4c241aaea944db89ce143ca915c2d745c642216926d2ca63a7dc43ebd9f&")
     .setTimestamp()
     .setFooter({ text: "Page 2/2" });
 
